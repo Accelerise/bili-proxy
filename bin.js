52ba70782656111ff31b5a57b952b470fa6ad66e
caiyuhao <caiyuhao@kuaishou.com>
2021-12-02 00:32:43 +0800

    feat: init

---
+++ bin.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+// eslint-disable-next-line
+require = require('esm')(module)
+module.exports = require('./command.js')
---
+++ command.js
@@ -0,0 +1,30 @@
+import Koa from 'koa';
+import Router from 'koa-router';
+import compose from 'koa-compose';
+
+const router = new Router();
+
+router
+    .get('/test', async function test(ctx) {
+        ctx.body = {
+          url: ctx.url,
+          params: ctx.params
+        };
+    });
+
+const port = process.env.PORT || 8091;
+const server = new Koa();
+
+server.use(async (ctx, next) => {
+   ctx.body = {
+      url: ctx.url,
+      params: ctx.params || {},
+      query: ctx.query
+   };
+})
+// server.use(compose([router.routes(), router.allowedMethods()]));
+
+server.listen(port);
+
+/* eslint-disable-next-line */
+console.log(`Server is listening on port ${port} 👏`)
